11150c1c4b79a40ed2593af42389971fe5f30029
nalvac <vacna1997@gmail.com>
2024-07-20 19:31:43 +0200

    activity list added

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -23,24 +23,52 @@ List<City> cities = [
 
 List<Activity> activities = [
   Activity(
-    image: 'assets/images/activities/louvre.jpeg',
+    image: 'assets/images/activities/louvre.jpg',
     name: 'Louvre',
     id: 'a1',
     city: 'Paris',
     price: 12.00,
   ),
   Activity(
-    image: 'assets/images/activities/chaumont.jpeg',
+    image: 'assets/images/activities/chaumont.jpg',
     name: 'Chaumont',
     id: 'a2',
     city: 'Paris',
     price: 0.00,
   ),
   Activity(
-    image: 'assets/images/activities/dame.jpeg',
+    image: 'assets/images/activities/dame.jpg',
     name: 'Notre-Dame',
     id: 'a3',
     city: 'Paris',
     price: 0.00,
+  ),
+  Activity(
+    image: 'assets/images/activities/nice_orthodox.jpg',
+    name: 'Eglise orthodoxe',
+    id: 'n1',
+    city: 'Nice',
+    price: 5.00,
+  ),
+  Activity(
+    image: 'assets/images/activities/nice_riviera.jpg',
+    name: 'Riviera',
+    id: 'n2',
+    city: 'Nice',
+    price: 0.00,
+  ),
+  Activity(
+    image: 'assets/images/activities/nice_promenade.jpg',
+    name: 'Promenade des Anglais',
+    id: 'n3',
+    city: 'Nice',
+    price: 0.00,
+  ),
+  Activity(
+    image: 'assets/images/activities/nice_opera.jpg',
+    name: 'Opéra',
+    id: 'n4',
+    city: 'Nice',
+    price: 100.00,
   ),
 ];
--- lib/views/city/city_view.dart
+++ lib/views/city/city_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:trips_app_flutter/views/city/activity_model.dart';
 import 'package:trips_app_flutter/data/data.dart' as data;
 
 class CityView extends StatefulWidget {
   CityView({super.key});
 
-  final List<Activity> activity = data.activities;
+  final List<Activity> activities = data.activities;
   static String routeName = '/city';
 
   @override
@@ -13,16 +14,44 @@ class CityView extends StatefulWidget {
 }
 
 class _CityViewState extends State<CityView> {
+  late int index;
+  DateTime? result;
 
-  void setDate() async {
-    final result = showDatePicker(
+  List<Activity> myTrips = [];
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  void switchIndex(nexIndex) {
+    setState(() {
+      index = nexIndex;
+    });
+  }
+
+  void setDate() {
+    showDatePicker(
       context: context,
       locale: const Locale("fr", "FR"),
       initialDate: DateTime.now().add(const Duration(days: 1)),
       firstDate: DateTime.now(),
       lastDate: DateTime(2025),
-    ).then((newDate) => {
-      if(newDate != null) {
+    ).then((value) {
+      setState(() {
+        result = value;
+      });
+    });
+  }
+
+  void addActivity(Activity activity) {
+    setState(() {
+      if (!myTrips.contains(activity)) {
+        myTrips
+            .add(activity);
+      } else {
+        myTrips.remove(activity);
       }
     });
   }
@@ -37,41 +66,131 @@ class _CityViewState extends State<CityView> {
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body:  Padding(
-          padding: EdgeInsets.all(10),
-          child: SizedBox(
-            height: 120,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Paris',
-                  style: TextStyle(
-                      fontSize: 18, decoration: TextDecoration.underline),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [ const Text('Choisissez une date'), ElevatedButton(onPressed: () { setDate(); }, child: Text('Selectionner une date'),)],
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SizedBox(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Paris',
+                style: TextStyle(
+                    fontSize: 18, decoration: TextDecoration.underline),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(result != null
+                      ? DateFormat("d/M/y").format(result!)
+                      : 'Choisissez une date'),
+                  ElevatedButton(
+                    onPressed: () {
+                      setDate();
+                    },
+                    child: Text('Selectionner une date'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Montant / Personnne',
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                    '0.0',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 1,
+                  crossAxisSpacing: 1,
                   children: [
-                    Text(
-                      'Montant / Personnne',
-                      style: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal),
+                    ...widget.activities.map(
+                      (activity) {
+                        return Card(
+                          elevation: 2,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Ink.image(
+                                image: AssetImage(activity.image),
+                                fit: BoxFit.cover,
+                                child: InkWell(
+                                  onTap: () {
+                                    addActivity(activity);
+                                  },
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(10),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        if (myTrips.contains(activity))
+                                          const Icon(Icons.check, color: Colors.white, size: 40,)
+                                      ],
+                                    ),
+                                    const Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Flexible(
+                                          child: FittedBox(
+                                            child: Text(
+                                              'Titre de lactivité',
+                                              style: TextStyle(
+                                                  fontSize: 20,
+                                                  color: Colors.white),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                    Text(
-                      '0.0',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    )
                   ],
-                )
-              ],
-            ),
-          )),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Découvert'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.stars), label: 'Mes activités')
+        ],
+        onTap: switchIndex,
+      ),
     );
   }
 }
